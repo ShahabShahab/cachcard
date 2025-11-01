480258bd0a18610b48b45cf6238f225972ce1114
s.Golpaiegan <shahabgolpaiegan@gmail.com>
2025-11-01 23:48:19 +0330

    refactor: change build to maven

---
+++ src/main/java/com/example/cachcard/CachCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cachcard/CachCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cachcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
